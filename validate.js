eda2756dc64ff9f6c38a249161e6329dcf8773bb
Vishesh <visheshsingh16@gmail.com>
2018-03-01 21:04:05 -0600

    testing a regex

--- validate.js
+++ validate.js
@@ -1,11 +1,30 @@
 // MAKING REGEX
 
 /* var regex = /[a-z]gi/;
-
 var regex2 = new RegExp(/[a-z]/, "ig"); */
+
+////////////////////////////////////////////
+const inputs = document.querySelectorAll('input');
 
 // REGEX FOR TELEPHONE
 
 const patterns = {
     telephone: /^\d{10}$/
 } 
+
+// VALIDATE FUNCTION
+function validate(field, regex){
+    //console.log(regex.test(field.value));
+    if(regex.test(field.value)){
+        field.className='valid;'
+    } else {
+        field.className='invalid;'
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
